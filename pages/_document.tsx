3c5a1b692751bd617bc26254828e48b0c9cc97b0
Brad Roodt <broodt@worldremit.com>
2024-08-15 07:03:44 +0200

    Move generated css to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,8 @@
 import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v13-pagesRouter';
 import { Html, Head, Main, NextScript } from "next/document";
+import {ServerStyleSheets} from "@mui/styles";
+import createCache from '@emotion/cache'
+import React from 'react';
 
 export default function Document(props:any) {
   return (
@@ -14,3 +17,35 @@ export default function Document(props:any) {
     </Html>
   );
 }
+
+
+
+
+Document.getInitialProps = async (ctx:any) => {  
+  const styledComponentsSheet = new ServerStyleSheets();
+
+  try {
+    const finalProps = await documentGetInitialProps(ctx, {
+      emotionCache: createCache({
+        key: 'mui'
+      }),
+      plugins: [
+        {
+          // styled-components
+          enhanceApp: (App) => (props) =>
+            styledComponentsSheet.collect(<App {...props} />),
+          resolveProps: async (initialProps) => ({
+            ...initialProps,
+            styles: [
+              styledComponentsSheet.getStyleElement(),
+              ...React.Children.toArray(initialProps.styles),
+            ],
+          }),
+        },        
+      ],
+    });
+    return finalProps;
+  } catch (error){
+    console.log(error);
+  }
+};
